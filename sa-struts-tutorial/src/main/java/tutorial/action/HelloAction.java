42389d6fa0edc2f33e9bf823c1dabf54e827644b
darkcat666 <holycat666@gmail.com>
2022-10-05 16:50:14 +0900

    plus-tutolial

---
+++ sa-struts-tutorial/src/main/java/tutorial/action/HelloAction.java
@@ -0,0 +1,12 @@
+package tutorial.action;
+
+import org.seasar.struts.annotation.Execute;
+
+public class HelloAction {
+
+    @Execute(validator = false)
+    public String index() {
+        return "index.jsp";
+    }
+    
+}
